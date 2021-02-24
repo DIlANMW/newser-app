5a1a74e14ab514bfe6fc36cfb2d32340a6f6622c
dilan weerasinghe <dilanweerasinghe97@outlook.com>
2021-02-24 21:04:59 +0530

    First Commit☺

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Newscard } from './components/Newscard';
+import { Navbar } from './components/navbar';
+import { Gallery } from './components/gallery';
+import { Footer } from './components/footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'c36e8106f98b4822995d69462af3957e';
+const API_URL = 'http://newsapi.org/v2/everything';
+const SORTBY = 'popularity';
+const DATE = '2021 - 02 - 18';
+class Search extends Component {
+  state = {
+    query: '',
+    results: [],
+  };
+
+  getInfo = () => {
+    axios
+      .get(
+        `${API_URL}?` +
+          `q=${this.state.query}&` +
+          `from=${DATE}&` +
+          `sortBy=${SORTBY}&` +
+          `apiKey=${API_KEY}`
+      )
+
+      .then(({ data }) => {
+        this.setState({
+          results: data.articles,
+        });
+
+        // console.log(data.articles);
+      });
+  };
+
+  handleInputChange = (e) => {
+    if (this.search.value !== null) {
+      this.setState(
+        {
+          query: this.search.value,
+        },
+        () => {
+          if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 3 === 0) {
+              this.getInfo();
+            }
+          }
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Gallery />
+        <form className='container '>
+          <div className='row d-flex  justify-content-center  '>
+            <input
+              type='search'
+              placeholder='Search News..'
+              className='form-control  
+              form-control-lg  align-self-center w-50'
+              ref={(input) => (this.search = input)}
+              onKeyPress={this.handleInputChange}
+            />
+          </div>
+        </form>
+        <br />
+        {this.state.results.map((value) => (
+          <Newscard
+            key={value.url}
+            Title={value.title}
+            content={value.content}
+            img={value.urlToImage}
+            author={value.author}
+            date={value.publishedAt}
+          />
+        ))}
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Search;
